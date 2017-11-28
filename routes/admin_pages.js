d053c9ca0721f187ded7e954d8631798052fef7d
Mark Geller <markg@condukia.com>
2017-11-28 14:24:28 -0800

    Added post for pages

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res) {
 });
 
 /**
- * Get pages index
+ * Get add page
  */
 router.get('/add-page', function(req, res) {
   var title = "";
@@ -21,6 +21,36 @@ router.get('/add-page', function(req, res) {
     slug: slug,
     content: content
   });
+});
+
+/**
+ * POST add page
+ */
+router.post('/add-page', function(req, res) {
+
+  req.checkBody('title', 'Title must have a value').notEmpty();
+  req.checkBody('content', 'Content must have a value').notEmpty();
+
+  var title = req.body.title;
+  var slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
+  if (slug == "") slug = title.replace(/\s+/g, '-').toLowerCase();
+  var content = req.body.content;
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    console.log(errors);
+    res.render('admin/add_page', {
+      errors: errors,
+      title: title,
+      slug: slug,
+      content: content
+    });
+  } else {
+    console.log('success');
+  }
+
+
 });
 
 // Exports
